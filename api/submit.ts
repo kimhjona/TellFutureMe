03d5ed16d96bb66bde56ffc2a23780db0d31c29a
kimhjona <kimhjona@gmail.com>
2025-02-22 11:57:40 -0800

    trigger another build

--- api/submit.ts
+++ api/submit.ts
@@ -30,7 +30,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     }
 
     // // Send confirmation email
-    // await sendConfirmationEmail(email, fileName, deliveryDate);
 
     // Return success
     return res.status(200).json({ success: true, fileName });
