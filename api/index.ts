589e85b7312876304eac9cfa4d04e5acaea50ab0
kimhjona <kimhjona@gmail.com>
2025-02-22 17:12:58 -0800

    testing vercel

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-const path = require("path");
+import path from "path";
 
 const app = express();
 
@@ -68,4 +68,4 @@ app.get("/", (req, res) => {
   });
 })();
 
-module.exports = app;
+export default app;
